dded481d26fa46cac6f46a215051a54720bf3e00
rushilshakya <rushil.shakya@gmail.com>
2024-06-18 13:00:29 +0545

    chore: correct copy

--- components/Menu.js
+++ components/Menu.js
@@ -50,12 +50,12 @@ export const Menu = () => {
       </div>
       <article className="[ bg-img-vector ] [ nav-grid-hero ]">
         <div className="[ center-text-section ] [ flow wrapper ] [ overlay ] ">
-          <div className="text-800 font-serif weight-black color-white">
-            MENTAL HEALTH HUB
+          <div className="text-800 font-serif weight-black color-white text-transform-uppercase">
+            Asian Mental Health Hub
           </div>
           <div className="text-500 md:text-600 weight-bold color-white">
-            Lobortis morbi lectus in varius lectus. Dictumst dui at quam iaculis
-            interdum. Eget sem neque eget ut venenatis duis.
+            Helping mental health providers bring culturally-competent care to
+            Asian Americans
           </div>
         </div>
       </article>
