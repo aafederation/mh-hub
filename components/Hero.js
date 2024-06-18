6950a1b6087a1921428329fcc615f6c67afeb3e9
rushilshakya <rushil.shakya@gmail.com>
2024-06-18 21:31:14 +0545

    refactor: hero

---
+++ components/Hero.js
@@ -0,0 +1,15 @@
+export const Hero = ({ resources }) => {
+  return (
+    <article className="[ bg-img-vector ] [ nav-grid-hero ]">
+      <div className="[ center-text-section ] [ flow wrapper ] [ overlay ] ">
+        <div className="text-800 font-serif weight-black color-white text-transform-uppercase">
+          Asian Mental Health Hub
+        </div>
+        <div className="text-500 md:text-600 weight-bold color-white">
+          Helping mental health providers bring culturally-competent care to
+          Asian Americans
+        </div>
+      </div>
+    </article>
+  );
+};
