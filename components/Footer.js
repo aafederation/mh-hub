91aa46b9a8ea93a2df0bd7f0bd90cb44398cc9b9
rushilshakya <rushil.shakya@gmail.com>
2024-06-19 22:48:59 +0545

    refactor: move sponsor to footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,28 +1,39 @@
 export const Footer = () => {
   return (
-    <div className="bg-dark gap-top-300 pad-top-500 pad-bottom-500">
-      <div className="wrapper footer-tiles">
-        <div className="pad-left-300 pad-right-300 pad-top-300 pad-bottom-300">
-          <img
-            width="1"
-            height="1"
-            src="/img/logo/AAF-Logo-White.svg"
-            className="image gap-left-0 pad-bottom-500"
-            alt=""
-            loading="lazy"
-          />
-          <p className="color-slate">
-            The Asian American Federation’s mission is to raise the influence
-            and well-being of the pan-Asian American community through research,
-            policy advocacy, public awareness and organizational development.
-          </p>
+    <div>
+      <div className="[  index-blurb flow ] [ pad-top-1000 pad-right-500 pad-bottom-1000 pad-left-500 bg-canvas ]">
+        <p className="text-500 weight-bold ta-center">
+          AAF's Mental Health Hub was made possible with support from
+        </p>
+        <div className="wrapper" data-variant="narrow">
+          <img src="/img/logo/MotherCabriniLogo.png" />
         </div>
-        <div className="pad-left-300 pad-right-300 pad-top-400 pad-bottom-300">
-          <p className="color-slate">120 Wall Street, 9th Floor</p>
-          <p className="color-slate">New York, NY 10005</p>
-        </div>
-        <div className="pad-left-300 pad-right-300 pad-top-1000">
-          <p className="color-slate">© 2022 Asian American Federation</p>
+      </div>
+      <div className="bg-dark gap-top-300 pad-top-500 pad-bottom-500">
+        <div className="wrapper footer-tiles">
+          <div className="pad-left-300 pad-right-300 pad-top-300 pad-bottom-300">
+            <img
+              width="1"
+              height="1"
+              src="/img/logo/AAF-Logo-White.svg"
+              className="image gap-left-0 pad-bottom-500"
+              alt=""
+              loading="lazy"
+            />
+            <p className="color-slate">
+              The Asian American Federation’s mission is to raise the influence
+              and well-being of the pan-Asian American community through
+              research, policy advocacy, public awareness and organizational
+              development.
+            </p>
+          </div>
+          <div className="pad-left-300 pad-right-300 pad-top-400 pad-bottom-300">
+            <p className="color-slate">120 Wall Street, 9th Floor</p>
+            <p className="color-slate">New York, NY 10005</p>
+          </div>
+          <div className="pad-left-300 pad-right-300 pad-top-1000">
+            <p className="color-slate">© 2022 Asian American Federation</p>
+          </div>
         </div>
       </div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -26,14 +26,6 @@ export default function Home({ result, posts }) {
       >
         <MainBody homeData={data.page} posts={postData.postConnection.edges} />
       </div>
-      <div className="[  index-blurb flow ] [ pad-top-1000 pad-right-500 pad-bottom-1000 pad-left-500 bg-canvas ]">
-        <p className="text-500 weight-bold ta-center">
-          AAF's Mental Health Hub was made possible with support from
-        </p>
-        <div className="wrapper" data-variant="narrow">
-          <img src="/img/logo/MotherCabriniLogo.png" />
-        </div>
-      </div>
     </Layout>
   );
 }
