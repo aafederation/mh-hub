65cd7bf450cc7b9d3798940becbbecc204d84120
rushilshakya <rushil.shakya@gmail.com>
2024-06-19 21:34:32 +0545

    style: fix hero styling

--- components/PostHero.js
+++ components/PostHero.js
@@ -1,26 +1,33 @@
 import { tinaField } from "tinacms/dist/react";
 
-export const PostHero = ({ homeData }) => {
+export const PostHero = ({ homeData, postData }) => {
   return (
-    <article
-      className="[ bg-img-vector ] [ nav-grid-hero ]"
-      data-tina-field={tinaField(homeData, "image")}
-      style={{ backgroundImage: `url(${homeData.image})` }}
-    >
-      <div className="[ center-text-section ] [ flow wrapper ] [ overlay ] ">
+    <div className="display-grid">
+      <article
+        className="[ bg-post-img-vector display-grid-row-1-20 ]"
+        data-tina-field={tinaField(postData, "image")}
+        style={{
+          backgroundImage: `url(${
+            postData.image?.length > 1 ? postData.image : homeData.image
+          })`,
+        }}
+      >
+        <div className="overlay"></div>
+      </article>
+      <div className="display-grid-row-10-11 text-align-center pad-left-500 pad-right-500">
         <div
-          className="text-800 font-serif weight-black color-white text-transform-uppercase"
+          className="text-600 md:text-800 font-serif weight-black color-white text-transform-uppercase"
           data-tina-field={tinaField(homeData, "heading")}
         >
           {homeData.heading}
         </div>
         <div
-          className="text-500 md:text-600 weight-bold color-white"
+          className="text-400 md:text-600 weight-bold color-white"
           data-tina-field={tinaField(homeData, "subHeading")}
         >
           {homeData.subHeading}
         </div>
       </div>
-    </article>
+    </div>
   );
 };
