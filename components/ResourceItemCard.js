5b1f525f2b8b5f999c13f481b117315a9bc387a1
rushilshakya <rushil.shakya@gmail.com>
2024-06-19 12:26:32 +0545

    feat: update posts tinafield

--- components/ResourceItemCard.js
+++ components/ResourceItemCard.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
+import { tinaField } from "tinacms/dist/react";
 
 export const ResourceItemCard = ({ resource }) => {
   return (
     <>
       <section className="card-resource-item [ pad-top-500 pad-bottom-500 pad-left-600 pad-right-600 ]">
-        <div className="member_img display-grid">
+        <div
+          className="member_img display-grid"
+          data-tina-field={tinaField(resource, "image")}
+        >
           <Image
             src={
               resource?.image?.length > 1 ? resource.image : "/img/blank.svg"
@@ -14,18 +18,36 @@ export const ResourceItemCard = ({ resource }) => {
             height={300}
             className="display-grid-row-1-20"
           />
-          <div className="display-grid-row-18-20">
+          <div
+            className="display-grid-row-18-20"
+            data-tina-field={tinaField(resource, "typeOfResource")}
+          >
             <span className="resource-type-display gap-left-300 gap-bottom-300">
               {resource?.typeOfResource}
             </span>
           </div>
         </div>
-        <span className="date">{resource?.date}</span>
-        <h3 className="pad-top-300">{resource?.title}</h3>
-        <span className="tag pad-top-300">
+        <span className="date" data-tina-field={tinaField(resource, "date")}>
+          {resource?.date}
+        </span>
+        <h3
+          className="pad-top-300"
+          data-tina-field={tinaField(resource, "title")}
+        >
+          {resource?.title}
+        </h3>
+        <span
+          className="tag pad-top-300"
+          data-tina-field={tinaField(resource, "tags")}
+        >
           Tag: {resource?.tags?.join(", ")}
         </span>
-        <p className="pad-top-300">{resource?.summary}</p>
+        <p
+          className="pad-top-300"
+          data-tina-field={tinaField(resource, "summary")}
+        >
+          {resource?.summary}
+        </p>
       </section>
     </>
   );
