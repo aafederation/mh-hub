127cd349ec99b609593884c32f77363dd6e8c9c3
rushilshakya <rushil.shakya@gmail.com>
2024-06-19 15:28:10 +0545

    feat: fix #2

--- components/LandingCategory.js
+++ components/LandingCategory.js
@@ -4,12 +4,12 @@ export const LandingCategory = ({ categoryDetails, anchor }) => {
   return (
     <>
       <h2
-        className="pad-left-800 gap-top-500 text-transform-uppercase"
+        className="pad-left-500 gap-top-500 text-transform-uppercase"
         id={anchor}
       >
         {categoryDetails.title}
       </h2>
-      <div className="mhd-tiles">
+      <div className="display-grid grid-3up justify-items-center align-items-start">
         {categoryDetails.items.map((resource, i) => (
           <ResourceItemCard resource={resource.node} key={i} />
         ))}
--- components/ResourceItemCard.js
+++ components/ResourceItemCard.js
@@ -3,66 +3,58 @@ import { tinaField } from "tinacms/dist/react";
 
 export const ResourceItemCard = ({ resource }) => {
   return (
-    <>
-      <section className="card-resource-item [ pad-top-500 pad-bottom-500 pad-left-600 pad-right-600 ]">
+    <section className="card-resource-item [ pad-top-500 pad-bottom-500 pad-left-600 pad-right-600 ]">
+      <div
+        className="member_img display-grid"
+        data-tina-field={tinaField(resource, "image")}
+      >
+        <Image
+          src={resource?.image?.length > 1 ? resource.image : "/img/blank.svg"}
+          alt="our_team"
+          width={300}
+          height={300}
+          className="display-grid-row-1-20"
+        />
         <div
-          className="member_img display-grid"
-          data-tina-field={tinaField(resource, "image")}
+          className="display-grid-row-18-20"
+          data-tina-field={tinaField(resource, "typeOfResource")}
         >
-          <Image
-            src={
-              resource?.image?.length > 1 ? resource.image : "/img/blank.svg"
-            }
-            alt="our_team"
-            width={300}
-            height={300}
-            className="display-grid-row-1-20"
-          />
-          <div
-            className="display-grid-row-18-20"
-            data-tina-field={tinaField(resource, "typeOfResource")}
-          >
-            <span className="resource-type-display gap-left-300 gap-bottom-300">
-              {resource?.typeOfResource}
-            </span>
-          </div>
+          <span className="resource-type-display gap-left-300 gap-bottom-300">
+            {resource?.typeOfResource}
+          </span>
         </div>
-        <span className="date" data-tina-field={tinaField(resource, "date")}>
-          {new Date(resource?.date).toLocaleDateString("en-us", {
-            day: "2-digit",
-            month: "short",
-            year: "numeric",
-          })}
-        </span>
-        <h3
-          className="pad-top-300 text-transform-capitalize"
-          data-tina-field={tinaField(resource, "title")}
-        >
-          {resource.type === "link" ? (
-            <a
-              href={resource?.linkUrl}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {resource?.title}
-            </a>
-          ) : (
-            <a href={`/post/${resource._sys.filename}`}>{resource?.title}</a>
-          )}
-        </h3>
-        <span
-          className="tag pad-top-300"
-          data-tina-field={tinaField(resource, "tags")}
-        >
-          Tag: {resource?.tags?.join(", ")}
-        </span>
-        <p
-          className="pad-top-300"
-          data-tina-field={tinaField(resource, "summary")}
-        >
-          {resource?.summary}
-        </p>
-      </section>
-    </>
+      </div>
+      <span className="date" data-tina-field={tinaField(resource, "date")}>
+        {new Date(resource?.date).toLocaleDateString("en-us", {
+          day: "2-digit",
+          month: "short",
+          year: "numeric",
+        })}
+      </span>
+      <h3
+        className="pad-top-300 text-transform-capitalize"
+        data-tina-field={tinaField(resource, "title")}
+      >
+        {resource.type === "link" ? (
+          <a href={resource?.linkUrl} target="_blank" rel="noopener noreferrer">
+            {resource?.title}
+          </a>
+        ) : (
+          <a href={`/post/${resource._sys.filename}`}>{resource?.title}</a>
+        )}
+      </h3>
+      <span
+        className="tag pad-top-300"
+        data-tina-field={tinaField(resource, "tags")}
+      >
+        Tag: {resource?.tags?.join(", ")}
+      </span>
+      <p
+        className="pad-top-300"
+        data-tina-field={tinaField(resource, "summary")}
+      >
+        {resource?.summary}
+      </p>
+    </section>
   );
 };
