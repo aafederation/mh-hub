9885cecfb0fb370e56d258183c54ffb8cc321863
rushilshakya <rushil.shakya@gmail.com>
2024-06-19 22:51:01 +0545

    feat: styling on single post

--- components/SinglePost.js
+++ components/SinglePost.js
@@ -1,60 +1,39 @@
-import Image from "next/image";
 import { tinaField } from "tinacms/dist/react";
+import { TinaMarkdown } from "tinacms/dist/rich-text";
 
 export const SinglePost = ({ postData }) => {
   return (
-    <section className="card-resource-item [ pad-top-500 pad-bottom-500 pad-left-600 pad-right-600 ]">
-      <div
-        className="member_img display-grid"
-        data-tina-field={tinaField(postData, "image")}
-      >
-        <Image
-          src={postData?.image?.length > 1 ? postData.image : "/img/blank.svg"}
-          alt="our_team"
-          width={300}
-          height={300}
-          className="display-grid-row-1-20"
-        />
-        <div
-          className="display-grid-row-18-20"
-          data-tina-field={tinaField(postData, "typeOfResource")}
-        >
-          <span className="resource-type-display gap-left-300 gap-bottom-300">
-            {postData?.typeOfResource}
-          </span>
-        </div>
+    <div className="wrapper single-post gap-bottom-1000">
+      <div className="pad-top-500 pad-bottom-500">
+        Home / Strategies to talk to my client / How to Support Your Child
       </div>
-      <span className="date" data-tina-field={tinaField(postData, "date")}>
-        {new Date(postData?.date).toLocaleDateString("en-us", {
-          day: "2-digit",
-          month: "short",
-          year: "numeric",
-        })}
+      <span className="date">
+        <span data-tina-field={tinaField(postData, "date")}>
+          {new Date(postData?.date).toLocaleDateString("en-us", {
+            day: "2-digit",
+            month: "short",
+            year: "numeric",
+          })}
+        </span>{" "}
+        |{" "}
+        <span data-tina-field={tinaField(postData, "tags")}>
+          {postData?.tags?.join(", ")}
+        </span>
       </span>
-      <h3
-        className="pad-top-300 text-transform-capitalize"
+      <h2
+        className="pad-top-300 pad-bottom-500 text-transform-capitalize"
         data-tina-field={tinaField(postData, "title")}
       >
-        {postData.type === "link" ? (
-          <a href={postData?.linkUrl} target="_blank" rel="noopener noreferrer">
-            {postData?.title}
-          </a>
-        ) : (
-          <a href={`/post/${postData._sys.filename}`}>{postData?.title}</a>
-        )}
-      </h3>
-      <span
-        className="tag pad-top-300"
-        data-tina-field={tinaField(postData, "tags")}
-      >
-        Tag: {postData?.tags?.join(", ")}
-      </span>
-      <p
-        className="pad-top-300"
-        data-tina-field={tinaField(postData, "summary")}
-      >
-        {postData?.summary}
-      </p>
-    </section>
+        {postData?.title}
+      </h2>
+      <TinaMarkdown
+        content={postData.body}
+        components={{
+          a: (props) => (
+            <a rel="nofollow" target="_blank" {...props} href={props.url} />
+          ),
+        }}
+      />
+    </div>
   );
 };
