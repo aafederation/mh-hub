3564d38473f0061d5d8ffde3ec87068c89411b71
rushilshakya <rushil.shakya@gmail.com>
2024-06-23 21:58:00 +0545

    feat: 404 page is home page

---
+++ pages/404.js
@@ -0,0 +1,31 @@
+import { Layout } from "../components/Layout";
+import { MainBody } from "../components/MainBody";
+import { Hero } from "@/components/Hero";
+import client from "@/tina/__generated__/client";
+import { useTina } from "tinacms/dist/react";
+
+export async function getStaticProps() {
+  const result = await client.queries.page({ relativePath: "home.md" });
+  const posts = await client.queries.postConnection();
+
+  return {
+    props: { result, posts },
+  };
+}
+
+export default function Home({ result, posts }) {
+  const { data } = useTina(result);
+  const { data: postData } = useTina(posts);
+
+  return (
+    <Layout>
+      <Hero homeData={data.page} />
+      <div
+        className="wrapper gap-top-1000 gap-bottom-1000"
+        data-variant="no-padding"
+      >
+        <MainBody homeData={data.page} posts={postData.postConnection.edges} />
+      </div>
+    </Layout>
+  );
+}
